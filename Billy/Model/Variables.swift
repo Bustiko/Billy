fc39d5a3a8f01e6605179dd83b4b51c78bfdcfde
Buse Karabıyık <karabiyikbuse04@gmail.com>
2023-09-26 19:46:43 +0300

    - moved file into another folder

---
+++ Billy/Model/Variables.swift
@@ -0,0 +1,16 @@
+//
+//  Variables.swift
+//  Billy
+//
+//  Created by Buse Karabıyık on 26.09.2023.
+//
+
+import UIKit
+
+struct Variables {
+    static var tipPercentage = 0
+    static var textField = UITextField()
+    static var stackView = UIStackView()
+    static var numberLabel = UILabel()
+    static var tipButtons : [UIButton] = []
+}
--- Billy/Variables.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Variables.swift
-//  Billy
-//
-//  Created by Buse Karabıyık on 26.09.2023.
-//
-
-import Foundation
